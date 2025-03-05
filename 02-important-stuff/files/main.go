445efba4d28454b981e9a537b023a9f280d92d8b
José Ferreira <dev.josejunior@gmail.com>
2025-03-04 22:44:29 -0300

    working with files

---
+++ 02-important-stuff/files/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Create("./test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	size, err := file.WriteString("Hello World\n")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("File created with size %d\n", size)
+
+	size2, err := file.Write([]byte("Second line\n"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("File updated with size %d\n", size2)
+	file.Close()
+
+	f, err := os.ReadFile("test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(f))
+
+	f2, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(f2)
+	buffer := make([]byte, 10)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Print(string(buffer[:n]))
+	}
+}
